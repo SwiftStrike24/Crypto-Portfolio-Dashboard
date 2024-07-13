26e758ecb6c8240a2864d7ec07b48f35f1d3c109
SwiftStrike24 <anwordavid@gmail.com>
2024-07-13 04:17:58 -0600

    Change output directory to 'build' for Vercel deployment

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'), // Change 'dist' to 'build'
     filename: 'bundle.js',
   },
   module: {
