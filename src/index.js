5d371d46a210e12ab3c7dd93acdb47ec745fcf83
SwiftStrike24 <anwordavid@gmail.com>
2024-07-14 20:58:00 -0600

    Add Vercel Speed Insights

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/tailwind.css';
 import { inject } from '@vercel/analytics'; // Import the inject function
+import { SpeedInsights } from '@vercel/speed-insights/react'; // Import the SpeedInsights component
 
 // Inject the Vercel Analytics script
 inject();
@@ -11,6 +12,7 @@ inject();
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <SpeedInsights /> {/* Add the SpeedInsights component */}
   </React.StrictMode>,
   document.getElementById('root')
 );
